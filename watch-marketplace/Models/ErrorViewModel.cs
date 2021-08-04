6e7d34a47dcabf6afd3ed0db7e304bad3cdd379a
Amar Tabakovic <tabakovicamar1@gmail.com>
2021-08-04 16:21:44 +0200

    Inital commit

---
+++ watch-marketplace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace watch_marketplace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
